e7793b72528ef5d3ee7fb034c73af1721da611b8
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-05-11 14:56:43 +0530

    hello world program is the first ever program that is have written in c language

---
+++ program_files/hello_world.c
@@ -0,0 +1,7 @@
+/*This program is the one which the people usually write when they start learning a coding language i.e., the "Hello World" program*/
+#include<stdio.h>
+void main()
+{
+	printf("Hello world\n");
+	//here the print function outputs "Hello World" string statement to the output console
+}
