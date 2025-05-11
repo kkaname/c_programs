b5626ba1285519a8da5aeb124301ab734e9d0d54
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-05-11 14:44:41 +0530

    removed the unnecesarry file

--- program_files/hello_world/hello_World.c
+++
@@ -1,7 +0,0 @@
-/*this program is the first ever program that people ususally write when they start learning a programming language, i.e., the basic hello world program*/
-#include<stdio.h>
-void main()
-{
-printf("Hello world\n");
-//here the print function outputs the "Hello World"  string statement to the output console
-}
