753a153ea1ee8df06c9cd9cfb50873ef34074445
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-05-21 20:02:25 +0530

    just named the file name to normal 'main.c' to follow simplicity instead of complex file names

---
+++ program_files/hello_world/main.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+/*This the basic hello world program that prople write when they start to learn a programming language, that just prints "Hello World" statement to the output console*/
+void main()
+{
+	printf("Hello World!!\n");
+}
