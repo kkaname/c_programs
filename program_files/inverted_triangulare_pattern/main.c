ab9aedd0c2c9f0678f174ba48032883a5499359d
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-07-07 14:55:15 +0530

    wrote a alternate way to print the inverted triangular pattern using the concept of rows and column under comment

--- program_files/inverted_triangulare_pattern/main.c
+++ program_files/inverted_triangulare_pattern/main.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 
+/*alternate way:
+ * for(int row = i; row <= n; row++){
+ * 	int col = 1;
+ * 	while(col <= (n - row)){
+ * 		printf(" ");
+ * 	}
+ * 	while(col <= n){
+ * 		printf("*");
+ * 	}
+ *	printf("\n");
+ * }*/
+
+
+
+
 int main(){
 	int n;
 	printf("Enter a number to print inverted triangulare pattern: ");
