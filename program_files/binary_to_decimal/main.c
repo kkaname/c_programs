50cac382f4c97533c51cc0635a251f73ba69895c
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-07-08 12:06:20 +0530

    this program takes binary number as input and prints the decimal equivalent of the binary number

---
+++ program_files/binary_to_decimal/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+void to_decimal(int n){
+	int i = 0, ans = 0;
+	if(n == 0)
+		printf("0\n");
+	while(n != 0){
+		ans += (n%2)*pow(2, i);
+		n = n/10;
+		i++;
+	}
+	printf("%d\n", ans);
+}
+
+int main(){
+	int n;
+	printf("Enter a binary:");
+	scanf("%d", &n);
+	to_decimal(n);
+	return 0; 
+}
