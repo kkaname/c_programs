2d0fc101f11aacbfb4311a5df6129ee37ce37b10
ayush <ayushhegde2005@gmail.com>
2025-07-28 17:58:35 +0530

    added few comments to improved the readability of the program

--- program_files/binary_to_decimal/main.c
+++ program_files/binary_to_decimal/main.c
@@ -1,12 +1,14 @@
+//this program uses basic mathe to covert a given binary number to decimal number
 #include<stdio.h>
 #include<math.h>
-
+//if using gcc, the incluce -lm flag to link the math.h library
 
 
 void to_decimal(int n){
 	int i = 0, ans = 0;
 	if(n == 0)
 		printf("0\n");
+    //if the number is not equal to zero
 	while(n != 0){
 		ans += (n%2)*pow(2, i);
 		n = n/10;
