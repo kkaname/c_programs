fc1d0a2994501fa05aa2164c80cda1a7a7957948
ayush <ayushhegde2005@gmail.com>
2025-09-23 00:43:43 +0530

    this program compares two string that is given by the user and outputs whether they are same or not

---
+++ program_files/string_comparision/main.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int strComp(char s1[], char s2[]){
+    int i = 0;
+    while(s1[i] && s2[i]){
+        if(s1[i] != s2[i]){
+            break;
+        }
+        ++i;
+    }
+    return (s1[i] - s2[i]);
+}
+
+int main(){
+    char s1[50], s2[50];
+    int state;
+    printf("Enter two strings to compare them:\n");
+    scanf("%s%s", s1, s2);
+    state = strComp(s1, s2);
+    if(state == 0){
+        printf("Both the string are equal.\n");
+    }
+    else{
+        printf("%d: Both the string are not equal.\n", state);
+    }
+    return 0;
+}
+
