49174d2622b6dad0c4459b66f7bf63789f4d3228
ayush <ayushhegde2005@gmail.com>
2025-10-05 17:31:56 +0530

    Makefiles to create executable/assembly file and also edited the code a bit

--- program_files/tempurature_converstion_table/fahr_to_celsius/main_f_to_c.c
+++ program_files/tempurature_converstion_table/fahr_to_celsius/main_f_to_c.c
@@ -3,8 +3,9 @@
 
 int main()
 {
-	float fahr, celsius, lower, upper, step;
-	//now we decide the upper and lower bound for the calculation.
+	float celsius, fahr;
+    int lower, upper, step;
+
 	lower = 0;	//setting the lower limit for the temperature table.
 	upper = 300;	//setting the upper limit for the temperature table.
 	step = 20;	//step size for the temperature reading to range from the lower to upper limit.
@@ -18,20 +19,20 @@ int main()
 	printf("Fahrenhiet\tcelsius\n");
 	while(fahr <= upper)
 	{
-		celsius = (5.0/9.0)*(fahr - 32);    //this is the formulla to convert the fahrenhiet degree to celsius degree of 
-						    //temperature.
+		celsius = (5.0/9.0)*(fahr - 32);    
 		printf("%10.1f%11.2f\n", fahr, celsius);
 		fahr += step;	
 	}
-	/*for(fahr = 0, fahr <= upper, fahr++)   \\this is the alternate way of printing fahrenhiet to celcius conversion table
+
+	/*[This here is a alternate way to do this program]
+     * for(fahr = 0, fahr <= upper, fahr++)   
 	 * {
 	 * 	celsius = (5.0/9.0)*(fahr - 32);
 	 * 	printf("%3d%6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
 	 * 	}*/
-	while(step >= 0)	//This was unnecessary, but added a line to know the end of the table.
+	for(int i = 0; i <= step; i++)     //This is unnecessary, but just added a line to make the table pretty
 	{
 		printf("*");
-		step -= 1;
 	}
 	printf("\n");
 	return 0;
