8af58bc2760a826c77849e6a479bacb806531c72
ayush <ayushhegde2005@gmail.com>
2025-10-06 23:55:20 +0530

    added Makefile to create executable file and also made some minor changes with the code

--- program_files/num_of_blanks_tabs_newlines/counting_blanks_newlines_tabs/main.c
+++ program_files/num_of_blanks_tabs_newlines/counting_blanks_newlines_tabs/main.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 //this program counts the number of blank spaces, new line characters and tab characters present in the user input
-int main(){
+int main(void){
 	int c, num_of_blanks = 0, num_of_tabs = 0, num_of_newlines = 0;
 	while((c = getchar()) != EOF){
-		if(c == ' '){
+		if(c == ' ')
 			++num_of_blanks;
-		}
-		else if(c == '\t'){
+
+		else if(c == '\t')
 			++num_of_tabs;
-		}
-		else if(c == '\n'){
+
+		else if(c == '\n')
 			++num_of_newlines;
-		}
 	}
 	printf("\nIn your input, there are %d number of blanks, %d number of tabs and %d number of newline\n", num_of_blanks, num_of_tabs, num_of_newlines);
 	return 0;
