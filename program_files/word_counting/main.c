56ebf654b4a197e332ca502a52789b1fb1d7736e
ayush <ayushhegde2005@gmail.com>
2025-10-08 11:12:52 +0530

    removed the lines that counts the number of characters, blanks, tab charater and newline character and only only sticked to part of counting the words, cause its a program to just count words, not other characters and stuff

--- program_files/word_counting/main.c
+++ program_files/word_counting/main.c
@@ -1,27 +1,19 @@
 #include<stdio.h>
-//this program counts the number of word from the user input untill the end of line(EOF) is encountered and outputs the exact number of words to the output console
+
+/*This program counts the number of word from the user input untill the end of line(EOF) is encountered and outputs the exact number of words to the output console */
+
 #define IN 1
 #define OUT 0
 
-int main()
+int main(void)
 {
-	int c, state, num_of_words, num_of_char, num_of_newlines, num_of_tabs, num_of_blanks;
-	num_of_words = num_of_char = num_of_newlines = num_of_tabs = num_of_blanks = 0;
+	int c, state, num_of_words;
+	num_of_words = 0;
 	state = OUT;
 
 	while((c = getchar()) != EOF)
 	{
-		++num_of_char;
 		if(c == ' ' || c == '\t' || c == '\n'){
-			if(c == ' '){
-				++num_of_blanks;
-			}
-			else if(c == '\t'){
-				++num_of_tabs;
-			}
-			else{
-				++num_of_newlines;
-			}
 			if(state == IN){
 				state = OUT;
 				++num_of_words;
@@ -30,10 +22,10 @@ int main()
 		else
 			state = IN;
 	}
-    if (state == IN) //in case the inputs ends without training backspaces
+    if (state == IN) /*in case the inputs ends without training backspaces*/
         ++num_of_words;
 
 	printf("\n");
-	printf("From the input, there are %d words, %d characters, %d tab characters, %d new line character and %d blank spaces\n", num_of_words, num_of_char, num_of_tabs, num_of_newlines, num_of_blanks);
+	printf("From the given input, there are %d words.\n", num_of_words);
 	return 0;
 }
