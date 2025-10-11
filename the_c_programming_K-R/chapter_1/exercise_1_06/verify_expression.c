c91e768f9798e969250481248031532d63c53463
ayush <ayushhegde2005@gmail.com>
2025-10-11 15:56:01 +0530

    Exercise:1-06 -> program to verify that the exression getchar() != EOF is 0 or 1

---
+++ the_c_programming_K-R/chapter_1/exercise_1_06/verify_expression.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+/*Exercise 1-6: verifies the expression of getchar() != EOF.*/
+
+int main(void)
+{
+    printf("The expression getchar() != EOF gives: %d\n", getchar() != EOF);
+    return 0;
+}
