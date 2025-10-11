055cbb6f6118a72c383d2c5cd061de9ec76a302d
ayush <ayushhegde2005@gmail.com>
2025-10-11 15:48:49 +0530

    Exercise:1.04 -> Prints Celcius to Fahrenheit conversion table as earlier exercises, but in reverse orver from 300 to 0

---
+++ the_c_programming_K-R/chapter_1/exercise_1_05/celcius_fahrenheit.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/*Exercise 1-5: create a conversion  table of celcius to fahrenheit from 300^o to 0^o.*/
+
+int main(void)
+{
+    float fahr, celcius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Celsius\t\tFahrenheit\n");
+    printf("--------------------------\n");
+
+    celcius = upper;
+    while(celcius >= lower)
+    {
+        fahr = (9.0 / 5.0) * celcius - 32.0f;
+        printf("%3.0f\t\t%6.2f\n", celcius, fahr);
+        celcius = celcius - step;
+    }
+
+    /*Alternate way to do is by using for loop:
+     * >for(celcius = upper; celcius >= lower; celcius = celcius - step);
+     * >fahr = (9.0 / 5.0) * celcius - 32.0f;
+     * >printf("%3.0f\t\t%6.2f\n", celcius, fahr);
+     */
+
+    return 0;
+}
