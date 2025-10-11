87ef486539f42d9f7bdd78e553544c5c3411511b
ayush <ayushhegde2005@gmail.com>
2025-10-11 15:00:54 +0530

    Exercise:1.3 -> This program prints Fahrenheit to Celcius converstion table with heading above the table

---
+++ the_c_programming_K-R/chapter_1/exercise_1_03/fahrenheit_celcius.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/*Exercise 1-3: conversion table of fahrenheit to celcius degree. */
+
+int main(void)
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit\tCelsius\n");
+    printf("-----------------------\n");
+
+    fahr = lower;
+    while(fahr <= upper)
+    {
+        celsius = (5.0/9.0) * (fahr - 32);
+        printf("%3.0f\t\t%6.2f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    /*
+     * Alternate way is to use a for loop instead of while loop:
+     * >for(fahr = lower; fahr <= upper; fahr = fahr + step);
+     * >celsius = (5.0/9.0) * (fahr - 32);
+     * > printf("%3.0f\t\t%6.2f\n", fahr, celsius);
+     */
+
+    return 0;
+}
