f60b0915ed313eb9765be63e9b58df7094b10661
ayush <ayushhegde2005@gmail.com>
2025-10-13 05:24:28 +0530

    Exercise 1.15: program to re-write Exercise 1.3/1.4, to print the tempurature conversion table using a table

---
+++ the_c_programming_K-R/chapter_1/exercise_1_15/temp_func.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void fahr_to_celcius(float fahr)
+{
+    float celcius;
+    celcius = (5.0/9.0) * (fahr - 32);
+    printf("%3.0f\t\t%.2f\n", fahr, celcius);
+    return;
+}
+
+
+int main(void)
+{
+    int lower, upper, step;
+    float fahr;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("Fahrenheit\tCelcius\n-------------------------\n");
+    fahr = lower;
+    while(fahr <= upper){
+        fahr_to_celcius(fahr);
+        fahr += step;
+    }
+
+    return 0;
+}
