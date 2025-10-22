74387561d3e474c5f0c023af4cc9b8e1d6e3172c
Ayush Hegde <ayushhegde2005@gmail.com>
2025-10-22 14:27:04 +0530

    corrected the logic of the code

--- the_c_programming_K-R/chapter_1/exercise_1_20/detab.c
+++ the_c_programming_K-R/chapter_1/exercise_1_20/detab.c
@@ -9,7 +9,8 @@ int main(void)
     int c, current_col, tab_stop;
     unsigned int no_spaces;
 
-    current_col = 1;
+    tab_stop = 1;
+    current_col = 0;
     while((c = getchar()) != EOF)
     {
         if (c == '\t'){
@@ -18,15 +19,14 @@ int main(void)
                 putchar(' ');
                 --no_spaces;
                 ++current_col;
-
             }
 
         }
         else {
-            ++current_col;
             putchar(c);
+            ++current_col;
             if (c == '\n'){
-                current_col = 0;
+                ++current_col;
             }
         }
     }
