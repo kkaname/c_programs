a7d698e4ae0e9ad353a8d4fd7d5b2b20d586d325
Ayush Hegde <ayushhegde2005@gmail.com>
2025-10-23 12:20:08 +0530

    modified the comment

--- program_files/hello_world/main.c
+++ program_files/hello_world/main.c
@@ -1,4 +1,4 @@
-/*This the basic hello world program that prople write when they start to learn a programming language, that just prints "Hello World" statement to the output console*/
+/*This the basic hello world program that people write when they start to learn a programming language, that just prints "Hello World" statement to the output console*/
 #include<stdio.h>
 
 int main()
