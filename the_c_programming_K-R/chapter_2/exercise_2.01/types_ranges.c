468f0609e162b62cf222ef7bd7fa6a3f58a0c913
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-03 19:17:44 +0530

    Added display for floating-point type ranges and refine output formatting.

--- the_c_programming_K-R/chapter_2/exercise_2.01/types_ranges.c
+++ the_c_programming_K-R/chapter_2/exercise_2.01/types_ranges.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<limits.h>
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
 
 /*Exercise 2.01: Program to determine the ranges of char, short, int, and long variables, both signed and unsigned, by printing appropriate values from standard headers and by direct computation.*/
 //NOTE: Harder if you compute them: determine the ranges of the various floating-point types.
@@ -13,36 +14,41 @@ int main(void)
     printf("Minimum value of char: %d\n", CHAR_MIN);
     printf("Maximum value of signed char: %d\n", SCHAR_MAX);
     printf("Minimum value of signed char: %d\n", SCHAR_MIN);
-    printf("Maximum value of unsigned char: %d\n", UCHAR_MAX);
+    printf("Maximum value of unsigned char: %d\n\n", UCHAR_MAX);
     putchar('\n');
 
     //to display the ranges of INT
     printf("########## INT ##########\n");
     printf("Maximum value of int: %d\n", INT_MAX);
     printf("Minimum value of int: %d\n", INT_MIN);
-    printf("Maximum value of unsigned int: %u\n", UINT_MAX);
-    putchar('\n');
+    printf("Maximum value of unsigned int: %u\n\n", UINT_MAX);
 
     //to display short int
     printf("########## SHORT INT ##########\n");
     printf("Maximum value of short: %d\n", SHRT_MAX);
     printf("Minimum value of short: %d\n", SHRT_MIN);
-    printf("Maximum value of unsigned short: %u\n", USHRT_MAX);
-    putchar('\n');
+    printf("Maximum value of unsigned short: %u\n\n", USHRT_MAX);
     
     //to display long int.
     printf("########## LONG INT ##########\n");
     printf("Maximum value of long: %ld\n", LONG_MAX);
     printf("Minimum value of long: %ld\n", LONG_MIN);
-    printf("Maximum value of unsigned long: %lu\n", ULONG_MAX);
-    putchar('\n');
+    printf("Maximum value of unsigned long: %lu\n\n", ULONG_MAX);
 
     //to display long long int
     printf("################ LONG LONG INT #################\n");
     printf("unsigned long long int max: %llu\n", ULLONG_MAX);
     printf("signed long long int min: %lld\n", LLONG_MIN);
-    printf("signed long long int max: %lld\n", LLONG_MAX);
-    printf("\n");
+    printf("signed long long int max: %lld\n\n", LLONG_MAX);
+    
+    //to display flaoting type numbers
+    printf("################ FLOAT PRECISION NUMBERS #################\n");
+    printf("float min: %e\n", FLT_MIN);
+    printf("float max: %e\n", FLT_MAX);
+    printf("Double min: %e\n", DBL_MIN);
+    printf("Double max: %e\n", DBL_MAX);
+    printf("Long Double min: %Le\n", LDBL_MIN);
+    printf("Long Double max: %Le\n\n", LDBL_MAX);
 
     return 0;
 }
