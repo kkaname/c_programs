40fae17ccda10a5b6ca1f2cb91ab552e81b59640
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-08 13:53:47 +0530

    program to count the 1-bits in its interger argument

---
+++ program_files/bitcount/bitcount.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*program that counts the number of 1-bits present in the binary representaion of its equivalent integer.*/
+
+int bitcount(unsigned x){
+    int rem, no_bits = 0;
+    while (x != 0){
+        rem = x % 2;
+        printf("%d\n", rem);
+        no_bits += rem;
+        x = x / 2;
+    }
+    return no_bits;
+}
+
+int main(void){
+    unsigned int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("no of 1-bits: %d\n", bitcount(x));
+
+    return 0;
+}
