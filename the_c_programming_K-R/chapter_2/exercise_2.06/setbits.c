a146c180eb1f90d7ab13bd6c84165f8b1f689b76
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-11 09:18:35 +0530

    Fix: changeed n to (p - (n-1)) in the right shift inside the setbits function, fixes a bug in the bit manipulation logic

--- the_c_programming_K-R/chapter_2/exercise_2.06/setbits.c
+++ the_c_programming_K-R/chapter_2/exercise_2.06/setbits.c
@@ -3,7 +3,7 @@
 /*Exercise 2-6: Write a function setbits(x, p, n, y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bitrs unchaged.*/
 
 int setbits(unsigned x, int p, int n, int y){
-    return (x & ~(~(~0 << n) << n)) | (y & ~(~0 << n)) << n;
+    return (x & ~(~(~0 << n) << n)) | (y & ~(~0 << n)) << (p - (n - 1));
 }
 
 
