6c1776e3d2c8e4aa1eb4b97fe858098ce621c231
Ayush Hegde <ayushhegde2005@gmail.com>
2025-12-11 09:26:26 +0530

    program to implement the strlen() function from the string.h library file

--- program_files/string_length/strlen.c
+++ program_files/string_length/strlen.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+/*Program to implement the strlen() funtion from the string.h library*/
+
 #define MAXLEN 100
 
-int str_len(char s[]){
-    int i;
+int str_len(char *s){
+    int i = 0;
     
     while (s[i] != '\0')
         ++i;
+
     return i;
 } 
 
 int main(void){
     char s[MAXLEN];
-    printf("Enter a string to determine its length:");
-    scanf("%s", s);
+    printf("Enter a string to determine its length: ");
+    scanf("%[^\n]%*c", s);
     printf("Length of string: %d\n", str_len(s));
+
     return 0;
 }
